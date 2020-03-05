a5eb2c650d6849ae7a20b0fe69b3e4e0af591b1b
Erik Sandberg <erik@repairtechsolutions.com>
2020-03-05 00:10:16 -0800

    cleanup

--- app/javascript/packs/ResourceGraph.jsx
+++ app/javascript/packs/ResourceGraph.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ReactDOM from "react-dom";
-import * as d3 from 'd3';
 import drawDag from './drawDag';
 import rd3 from 'react-d3-library';
 
@@ -21,35 +20,6 @@ class ResourceGraph extends React.Component {
         this.setState({d3: drawDag(document.createElement('div'), this.state.dagData)});
     }
 
-    example() {
-        var node = document.createElement('div');
-
-        var width = 960,
-            height = 500;
-
-        var svg = d3.select(node).append("svg")
-            .attr("width", width)
-            .attr("height", height);
-
-        var defs = svg.append("defs");
-
-        defs.append("clipPath")
-            .attr("id", "circle1")
-            .append("circle")
-            .attr("cx", 350)
-            .attr("cy", 200)
-            .attr("r", 180);
-
-        defs.append("clipPath")
-            .attr("id", "circle2")
-            .append("circle")
-            .attr("cx", 550)
-            .attr("cy", 200)
-            .attr("r", 180);
-
-        return node
-    }
-
     render() {
         return (
             <div>
@@ -57,24 +27,6 @@ class ResourceGraph extends React.Component {
             </div>
         )
     }
-
-    //
-    // render() {
-    //     const nodeRadius = this.props.nodeRadius;
-    //     const width = this.props.width;
-    //     const height = this.props.height;
-    //     const padding = this.props.padding;
-    //     const viewBox = [
-    //         -nodeRadius - padding,
-    //         -nodeRadius - padding,
-    //         width + (2 * nodeRadius) + (2 * padding),
-    //         height + (2 * nodeRadius) + (2 * padding),
-    //     ].join(' ');
-    //     return (
-    //         // <svg ref='svg' width={width} height={height} viewBox={viewBox}/>
-    //         <div>Hello Resource Graph</div>
-    //     );
-    // }
 }
 
 ResourceGraph.propTypes = {
@@ -93,8 +45,8 @@ ResourceGraph.defaultProps = {
 export default ResourceGraph
 
 document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(
-        <ResourceGraph/>,
-        document.getElementById('process-tree')
-    )
+    const parent = document.getElementById('process-tree');
+    if (parent) {
+        ReactDOM.render(<ResourceGraph/>, parent);
+    }
 });
